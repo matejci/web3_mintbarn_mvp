9480edb271eec85273b38f7954996f5bc110d5c6
matejci <ciloid@gmail.com>
2022-05-13 10:36:00 +0200

    Adding redirect to apple store

--- app/controllers/nfts_controller.rb
+++ app/controllers/nfts_controller.rb
@@ -4,6 +4,6 @@ class NftsController < ApplicationController
   skip_before_action :validate_app_token, :set_current_wallet_account, :set_current_chain
 
   def show
-    @nft = Nft.find(params[:id])
+    redirect_to('https://apps.apple.com/app/id1615549175', allow_other_host: true)
   end
 end
